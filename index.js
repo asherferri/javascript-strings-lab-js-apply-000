5d4f0bafbceba1b35c068b1d92212d8d561190ea
Asher Ferri <asherferri85@gmail.com>
2018-03-14 23:50:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Hello, ${specialGuest}!";
+var greetSpecialGuest = `Hello, ${specialGuest}!`;
 
 //(`Hello, ${specialGuest}!`)
 
 var topic = "space";
 
-var conversation = `Let's talk about ${topic}`;
+var conversation = `Let\'s talk about ${topic}`;
